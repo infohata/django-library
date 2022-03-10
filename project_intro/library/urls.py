ef3c3fd2bc0750f0a0eccfedeea6fd9525cbf671
Kestutis Januskevicius <infohata@gmail.com>
2022-03-10 21:58:36 +0200

    user registration

--- project_intro/library/urls.py
+++ project_intro/library/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('books/search/', views.search_books, name='books-search'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-books'),
+    path('register/', views.register, name='register'),
 ]
--- project_intro/library/views.py
+++ project_intro/library/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib import messages
 from django.core.paginator import Paginator
 from django.conf import settings
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views import generic
+from django.views.decorators.csrf import csrf_protect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from .models import Book, BookInstance, Author, Genre
 
 
@@ -94,3 +99,27 @@ def search_books(request):
         'query': query
     }
     return render(request, 'library/search_books.html', context=context)
+
+
+@csrf_protect
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if not (username and email and password1):
+            messages.error(request, _('Incomplete form, all fields are required.'))
+        elif password1 != password2:
+            messages.error(request, _('Passwords do not match.'))
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, _('Username already taken.'))
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, _('User with this email already exists.'))
+        else:
+            user = User(username=username, email=email)
+            user.set_password(password1)
+            user.save()
+            messages.success(request, _('User registration successful, you can login now.'))
+            return redirect(reverse_lazy('login'))
+    return render(request, 'library/register.html')
